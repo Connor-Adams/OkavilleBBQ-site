c61aa1e6f9b92b8033e21ace91e787611ab706a5
Connor Adams <connor.adams@uoit.net>
2025-03-31 17:20:11 -0400

    add dotenv

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import ContactPage from './pages/ContactPage';
 import AboutPage from './pages/AboutPage';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
+import 'dotenv/config';
+
 
 function App() {
 
