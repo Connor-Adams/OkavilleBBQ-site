9c8f39e4e4e86fa02ae537e4eaf9a9d2282c4dc3
Connor Adams <connor.adams@uoit.net>
2025-03-31 17:26:30 -0400

    change envvariablestocompleweithvite

--- src/util/supabaseClient.ts
+++ src/util/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://fmuubwhqarebjjoomyxr.supabase.co'; // Replace with your Supabase project URL
-const supabaseAnonKey = process.env.SUPABASEKEY || "";
+const supabaseAnonKey = process.env.VITE_SUPABASEKEY || "";
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
